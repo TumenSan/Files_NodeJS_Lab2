cc5b3d12932eda63ecba9e5e624caa682c346399
TumenSan <sandanov.tumen.sib@mail.ru>
2022-10-29 00:14:29 +0700

    com1.2

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(express.static(__dirname + "/public"));
   
 const filePath2 = "users.json";
 const filePath = "/FilesLab/";
-app.get("/api/files/create/:filename(*)", function(req, res){
+
+app.get("/api/file/create/:filename(*)", function(req, res){
     /*
     fs.open('testFile.txt', 'w', (err) => {
         if(err) throw err;
@@ -36,7 +37,21 @@ app.get("/api/files/create/:filename(*)", function(req, res){
         }
     });
 });
-app.delete("/api/files/delete/:filename(*)", function(req, res){
+app.get("/api/folder/create/:filename(*)", function(req, res){
+    /*
+    fs.open('testFile.txt', 'w', (err) => {
+        if(err) throw err;
+        console.log('File created');
+    });
+    */
+    let filename = "FilesLab/" + req.params.filename;
+    fs.mkdir(filename, err => {
+        if(err) throw err; // не удалось создать папку
+        console.log('Папка успешно создана');
+        res.send("папка создана2");
+    });
+});
+app.delete("/api/file/delete/:filename(*)", function(req, res){
     let filename = "FilesLab/" + req.params.filename;
     fs.unlink(filename, function(err){
         if (err) {
@@ -48,13 +63,36 @@ app.delete("/api/files/delete/:filename(*)", function(req, res){
         }
     });
 });
-app.get('/api/files/download/:file(*)', (req, res) => {
+app.delete("/api/folder/delete/:filename(*)", function(req, res){
+    let filename = "FilesLab/" + req.params.filename;
+    fs.rmdir(filename, err => {
+        if(err) throw err; // не удалось удалить папку
+        console.log('Папка успешно удалена');
+        res.send("папка удалена2");
+    });
+});
+app.get('/api/file/download/:file(*)', (req, res) => {
     let file = req.params.file;
     let fileLocation = path.join('./FilesLab', file);
 
     console.log(fileLocation);
     res.download(fileLocation);
     //res.download('./FilesLab', 'file123.txt');
+});
+app.get('/api/folder/count', (req, res) => {
+    /*
+    path1 = 'FilesLab/';
+    str = listObjects(path1);
+
+    console.log(str);
+    res.send(str);
+    */
+    let filename = "FilesLab/";
+    fs.readdir(filename, (err, files) => {
+        if(err) throw err; // не прочитать содержимое папки
+        console.log('В папке находятся файлы:' + files);
+        res.send(files);
+    });
 });
 app.get("/api/users", function(req, res){
        
@@ -163,6 +201,29 @@ app.put("/api/users", jsonParser, function(req, res){
         res.status(404).send(user);
     }
 });
+
+function listObjects(path){
+    let string1 = '';
+    fs.readdir(path, (err, files) => {
+       if(err) throw err;
+ 
+       for (let file of files){
+          fs.stat('file.txt', (errStat, status) => {
+             if(errStat) throw errStat;
+ 
+             if(status.isDerictory()){
+                string1 = string1 + 'Папка: ' + file;
+                console.log('Папка: ' + file);
+                listObjects(path + '/' + file); // продолжаем рекурсию
+             }else{
+                string1 = string1 + 'Файл: ' + file;
+                console.log('Файл: ' + file);
+             }
+          });
+       }
+    });
+    return string1;
+ }
    
 app.listen(3000, function(){
     console.log("Сервер ожидает подключения...");
